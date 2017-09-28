b3356d4653b44336553ae9989e48cc6ad9d5e92a
Gavin Cooper <gav.c.cooper@outlook.com>
2017-09-28 17:33:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ const app = "I don't do much.";
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(array, kitten) {
-  array.push(kitten);
-  return array;
+  return array.push(kitten);
 }
